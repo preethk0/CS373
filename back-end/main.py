aa851ba974e87558d5781b07535594b81ec2c7a8
Mehul Daruka <mehul.daruka@utexas.edu>
2021-09-30 18:38:09 -0500

    Changed up member responsibilities, added requirements.txt and changed to main.py

--- back-end/main.py
+++ back-end/main.py
@@ -3,7 +3,12 @@ import os
 from flask import Flask
 
 # create and configure the app
-app = Flask(__name__, instance_relative_config=True)
+app = Flask(
+    __name__,
+    static_folder="../frontend/build/static",
+    template_folder="../frontend/build",
+)
+
 app.config.from_mapping(
     SECRET_KEY='dev',
     DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite'),
