9fae3033a787e42812b98566d55efb236cc53ed7
Justin Lee <justin@Justins-Air.lan>
2021-10-02 21:00:40 -0500

    Added a basic table for the Geography viewall screen

--- front-end/src/screens/Geography/GeographyAll.js
+++ front-end/src/screens/Geography/GeographyAll.js
@@ -1,19 +1,42 @@
 import React from "react";
 import GeographyData from "./GeographyData";
+import * as Bootstrap from "react-bootstrap";
 
 const GeographyAll = ({}) => {
+
+  const getGeography = (country) => {
+    const geography = GeographyData[country];
+    return (
+      <tr key={geography.id}>
+        <td>{geography.country_name}</td>
+        <td>{geography.country_longitude}</td>
+        <td>{geography.country_latitude}</td>
+        <td>{geography.country_continent}</td>
+        <td>{geography.country_region}</td>
+      </tr>
+    )
+  }
+
   return (
     <div>
-      {Object.keys(GeographyData).map((country) => {
-        return (
-          <div>
-            <a href={"/geography/" + country}>{country}</a>
-            <br />
-          </div>
-        );
-      })}
+      <Bootstrap.Table table-bordered>
+        <thead>
+          <tr>
+            <th scope="col">Country</th>
+            <th scope="col">Longitutde</th>
+            <th scope="col">Latitude</th>
+            <th scope="col">Continent</th>
+            <th scope="col">Region</th>
+          </tr>
+        </thead>
+        <tbody>
+          {Object.keys(GeographyData).map(getGeography)}
+        </tbody>
+      </Bootstrap.Table>
     </div>
   );
 };
+
+{/* <a href={"/demographics/" + country}>{country}</a> */}
 
 export default GeographyAll;
