6f65d42ed1c247829a9ba59f6aed6a52569b902d
dany-torres <53352534+dany-torres@users.noreply.github.com>
2021-10-03 18:38:53 -0500

    added map image, will update this once goolge maps API works

--- front-end/src/screens/Geography/GeographyData.js
+++ front-end/src/screens/Geography/GeographyData.js
@@ -2,41 +2,41 @@ const GeographyData = {
     USA: {
         country_id: 1,
         country_name: "United States of America",
-        country_longitude: "37.0902° N",
-        country_latitude: "95.7129° W",
+        country_longitude: 37.0902,
+        country_latitude: 95.7129,
         country_continent: "America",
         country_region: "North America",
         country_land_area: "3,531,905 sq mi",
         country_water_area: "4.66%",
         country_adjacent_oceans: ["Atlantic Ocean", "Pacific Ocean"],
         country_adjacent_countries: ["Canada", "Mexico"],
-        country_map: "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d55565170.29301636!2d-132.08532758867793!3d31.786060306224!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x54eab584e432360b%3A0x1c3bb99243deb742!2sUnited%20States!5e0!3m2!1sen!2sus!4v1633146040770!5m2!1sen!2sus"
+        country_map: "https://www.google.com/maps/vt/data=BMan_-C3aNCrHDtjSaJWX_MP6IcvfQcb2nbruGPpDA6xaVNcQJJAWoDAd2_KOQ6591l51bXdBMWkdE1-DOu0ltDigDbldBqrr05aCktVdLD_yslAU2XiPZrXzkVuFt9D9BdmyCwYwxPy3rjMexfb5vgqbC26MRYIYDQYdelhOwlWT0zB6vaoJYi4VmTUDuou84zLDeZ8DaKbShP6KUD_5kq0hNTvjcQelfyFzsgWla5zzqgRPIuAKExs"
     },
     China: {
         country_id: 2,
-        country_name: "People's Republic of China",
-        country_longitude: "35.8617° N",
-        country_latitude: "104.1954° E",
+        country_name: "China",
+        country_longitude: 35.8617,
+        country_latitude: 104.1954,
         country_continent: "Asia",
         country_region: "East Asia",
         country_land_area: "3,705,407 sq mi",
         country_water_area: "2.8%",
         country_adjacent_oceans: ["Pacific Ocean"],
         country_adjacent_countries: ["Mongolia", "Kazakhstan", "North Korea", "Russia", "Kyrgyzstan", "Tajikistan", "Afghanistan", "Pakistan", "India", "Nepal", "Bhutan", "Myanmar", "Laos", "Vietnam"],
-        country_map: "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d56593509.36396439!2d67.4692816011634!3d30.03130070479193!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x31508e64e5c642c1%3A0x951daa7c349f366f!2sChina!5e0!3m2!1sen!2sus!4v1633146523275!5m2!1sen!2sus"
+        country_map: "https://www.google.com/maps/vt/data=4m2NwqGHHBDhe3HjGFAcVdQ-JQ_Kmzk7TcklrRFPM2EC6Gkqf7H-gxZVu6pHlN4idXW9a9i_7pzKuQ4mlYyRtHr-0_u8qIphfUXLqSJ_1cL7IbnNvIG8M-Aq0Lo9YdRWhyDTBqkc5EGgqPxZZbCfeHgSUQN6vOJybVR8Atxq1tq16dg37xJN7GHPa47fy0wCYQrOVbSK0eOW_tEQkx4b1rKWE5x2pXmlgwsVbNRwTl4pbbkEf3_LYUIJ"
     },
     India: {
         country_id: 3,
-        country_name: "Republic of India",
-        country_longitude: "20.5937° N",
-        country_latitude: "78.9629° E",
+        country_name: "India",
+        country_longitude: 20.5937,
+        country_latitude: 78.9629,
         country_continent: "Asia",
         country_region: "South Asia",
         country_land_area: "1,269,219 sq mi",
         country_water_area: "9.6%",
         country_adjacent_oceans: ["Indian Ocean"],
         country_adjacent_countries: ["China", "Bhutan", "Nepal", "Pakistan", "Bangladesh", "Myanmar"],
-        country_map: "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d61563344.98573597!2d63.185596131648516!3d19.08247797460803!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x30635ff06b92b791%3A0xd78c4fa1854213a6!2sIndia!5e0!3m2!1sen!2sus!4v1633146950110!5m2!1sen!2sus"
+        country_map: "https://www.google.com/maps/vt/data=pkWMF8kwCgwdg-I2dof7RXPFGahPvpBeSIitmNK-Ykm6iSQ_mgZyzKg_6mo_R5eL_BGuO8mzjgZ5PRnBJdt2JGV-BfJw9StzLbMbUK7TlqH2H6TRaI75Du2tAGRZbbkmich9N0jsLrIk2cJ8LkeLMp0bGzlrcIw7YKucr6MsULHVOoL6v1ICNK7MXiSbsVn4BnQLQJc3AgjSQpZdIRpxNCT-B7rMTt0aOUXapFPtZXW5QMseznlPXNXC"
     },
   };
   
