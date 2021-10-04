bf017eea96c86dcd7370dc6b2bd15093b71d2ffa
dany-torres <53352534+dany-torres@users.noreply.github.com>
2021-10-04 18:38:59 -0500

    fixed view all bar, and country Name size

--- front-end/src/screens/Demographics/DemographicsInstance.js
+++ front-end/src/screens/Demographics/DemographicsInstance.js
@@ -8,84 +8,84 @@ const DemographicsInstance = ({}) => {
   const data = DemographicsData[country];
 
   return (
-    <div className="mainPage">
-      <div class="row justify-content-center align-items-center" >
-      <div className="subNavBar">
-        <h1>Demographics of the World</h1>
-        <text>
-          <a href={"/demographics/"}>{"View All"}</a>
-        </text>
-      </div>
-      <div class="card-container">
-        <div class="card-body" >
-          <h1 class="card-title"> {data.country_name} </h1>
-          <img src={data.country_flag} className="flag" />
-        </div>
-        <div class= "card-text">
-        <p class="card-text" >
-            <b>Capital: </b>
-            {data.country_capital}
-          </p>
-          <p class="card-text">
-            <b>Population: </b>
-            {data.country_population.toLocaleString()}
-          </p>
-          <p class="card-text">
-            <b>World Share: </b>
-            {data.country_world_share}
-          </p>
-          <p class="card-text">
-            <b>Languages: </b>
-            {data.country_language.join(", ")}
-          </p>
-          <p class="card-text">
-            <b>Time Zones: </b>
-            {data.country_timeZones}
-          </p>
-          <p class="card-text">
-            <b>Religions: </b>
-            {data.country_religions.join(", ")}
-          </p>
-          <p class="card-text">
-            <b>GDP:</b> {data.country_GDP}
-          </p>
-          <p class="card-text">
-            <b>GDP Per Capita: </b>
-            {data.country_GDP_perCapita}
-          </p>
-          <p class="card-text">
-            <b>Currency: </b>
-            {data.country_currency}
-          </p>
-          <p class="card-text">
-            <b>Driving Side: </b>
-            {data.country_driving_side}
-          </p>
-          <p class="card-text">
-            <b>Calling Code: </b>
-            {data.country_calling_code}
-          </p>
-          <p class="card-text">
-            <b>Government Type: </b>
-            {data.country_gov_type}
-          </p>
-        </div>
-        <div className="linksToModules">
-          <h4> Interested to learn more about {data.country_name}?</h4>
-          <text>
-            Check out the <a href={"/geography/" + country}>{"Geography"}</a> of
-            this country
-          </text>
-          <br />
-          <text>
-            Check out the{" "}
-            <a href={"/foodandtourism/" + country}>{"Food and Tourism"}</a> of
-            this country
-          </text>
-        </div>
-        </div>
-      </div>
+    <><div className="subNavBar">
+      <h1>Demographics of the World</h1>
+      <text>
+        <a href={"/demographics/"}>{"View All"}</a>
+      </text>
     </div>
+    <div className="mainPage">
+        <div class="row justify-content-center align-items-center">
+          <div class="card-container">
+            <div class="card-body">
+              <h1 class="countryName"> {data.country_name} </h1>
+              <img src={data.country_flag} className="flag" />
+            </div>
+            <div class="card-text">
+              <p class="card-text">
+                <b>Capital: </b>
+                {data.country_capital}
+              </p>
+              <p class="card-text">
+                <b>Population: </b>
+                {data.country_population.toLocaleString()}
+              </p>
+              <p class="card-text">
+                <b>World Share: </b>
+                {data.country_world_share}
+              </p>
+              <p class="card-text">
+                <b>Languages: </b>
+                {data.country_language.join(", ")}
+              </p>
+              <p class="card-text">
+                <b>Time Zones: </b>
+                {data.country_timeZones}
+              </p>
+              <p class="card-text">
+                <b>Religions: </b>
+                {data.country_religions.join(", ")}
+              </p>
+              <p class="card-text">
+                <b>GDP:</b> {data.country_GDP}
+              </p>
+              <p class="card-text">
+                <b>GDP Per Capita: </b>
+                {data.country_GDP_perCapita}
+              </p>
+              <p class="card-text">
+                <b>Currency: </b>
+                {data.country_currency}
+              </p>
+              <p class="card-text">
+                <b>Driving Side: </b>
+                {data.country_driving_side}
+              </p>
+              <p class="card-text">
+                <b>Calling Code: </b>
+                {data.country_calling_code}
+              </p>
+              <p class="card-text">
+                <b>Government Type: </b>
+                {data.country_gov_type}
+              </p>
+            </div>
+            <div className="linksToModules">
+              <h4> Interested to learn more about {data.country_name}?</h4>
+              <text>
+                Check out the <a href={"/geography/" + country}>{"Geography"}</a> of
+                this country
+              </text>
+              <br />
+              <text>
+                Check out the{" "}
+                <a href={"/foodandtourism/" + country}>{"Food and Tourism"}</a> of
+                this country
+              </text>
+            </div>
+          </div>
+        </div>
+      </div></>
   );
 };
 
