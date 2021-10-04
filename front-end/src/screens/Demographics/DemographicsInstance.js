86925b06ecedd8f91f7dc8fab5751670070e43a6
dany-torres <53352534+dany-torres@users.noreply.github.com>
2021-10-04 12:15:13 -0500

    fixed data, display lists and large numbers with commas

--- front-end/src/screens/Demographics/DemographicsInstance.js
+++ front-end/src/screens/Demographics/DemographicsInstance.js
@@ -18,11 +18,11 @@ const DemographicsInstance = ({}) => {
               <img src = {DemographicsData[country].country_flag}/> 
               <p class="card-text"> <b> Country Name:</b> {DemographicsData[country].country_name} </p>
               <p class="card-text"> <b> Country Capital:</b> {DemographicsData[country].country_capital} </p>
-              <p class="card-text"> <b> Country Population:</b> {DemographicsData[country].country_population} </p>
+              <p class="card-text"> <b> Country Population:</b> {DemographicsData[country].country_population.toLocaleString()} </p>
               <p class="card-text"> <b> Country World Share:</b> {DemographicsData[country].country_world_share} </p>
-              <p class="card-text"> <b> Country Language:</b> {DemographicsData[country].country_language} </p>
+              <p class="card-text"> <b> Country Language:</b> {DemographicsData[country].country_language.join(", ")} </p>
               <p class="card-text"> <b> Country TimeZones:</b> {DemographicsData[country].country_timeZones} </p>
-              <p class="card-text"> <b> Country Religions:</b> {DemographicsData[country].country_religions} </p>
+              <p class="card-text"> <b> Country Religions:</b> {DemographicsData[country].country_religions.join(", ")} </p>
               <p class="card-text"> <b> Country GDP:</b> {DemographicsData[country].country_GDP} </p>
               <p class="card-text"> <b> Country GDP Per Capita:</b> {DemographicsData[country].country_GDP_perCapita} </p>
               <p class="card-text"> <b> Country Currency:</b> {DemographicsData[country].country_currency} </p>
