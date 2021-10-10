f1ec050cf1e343e7246d1e6f902656f2ffd989b5
preethk0 <preeth.kanamangala@gmail.com>
2021-10-10 18:04:06 -0500

    created db and config app to connect

--- back-end/main.py
+++ back-end/main.py
@@ -1,6 +1,12 @@
 import os
 
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import create_engine, Column, String, Integer
+import urllib
+import json
+from dotenv import load_dotenv
+
 
 # create and configure the app
 app = Flask(
@@ -9,10 +15,16 @@ app = Flask(
     template_folder="../frontend/build",
 )
 
+app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("AWS_DB_KEY")
+app.debug = True
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
 app.config.from_mapping(
     SECRET_KEY='dev',
     DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite'),
 )
+
+db = SQLAlchemy(app)
 
 # a simple page that says hello
 @app.route('/hello')
