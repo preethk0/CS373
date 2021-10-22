29491a14c590b1e69d4adb09be34fae3cbdeb07f
Mehul Daruka <mehul.daruka@utexas.edu>
2021-10-22 11:07:47 -0500

    geography border connections changes

--- front-end/src/screens/Geography/GeographyData.js
+++ front-end/src/screens/Geography/GeographyData.js
@@ -1,42 +1,47 @@
 const GeographyData = {
   NP: {
-    country_adjacent_countries: "['India']", 
-    country_continent: "Asia", 
-    country_id: "NP", 
-    country_land_area: "143,351 (55,348)", 
-    country_latitude: 28.0, 
-    country_longitude: 84.0, 
-    country_name: "Nepal", 
-    country_region: "South Asia", 
-    country_water_area: "3,830 (1,480)", 
+    country_adjacent_countries: "['India', 'China']",
+    country_continent: "Asia",
+    country_id: "NP",
+    country_land_area: "143,351 (55,348)",
+    country_latitude: 28.0,
+    country_longitude: 84.0,
+    country_name: "Nepal",
+    country_region: "South Asia",
+    country_water_area: "3,830 (1,480)",
     country_water_percent: "2.6",
-    country_topography_image: "https://www.worldofmaps.net/typo3temp/images/topographische-karte-nepal.jpg"
+    country_topography_image:
+      "https://www.worldofmaps.net/typo3temp/images/topographische-karte-nepal.jpg",
   },
   CN: {
-    country_adjacent_countries: "['Bhutan', 'Hong Kong', 'India', 'Kazakhstan', 'North Korea', 'Kyrgyzstan', 'Laos', 'Macao', 'Mongolia', 'Myanmar', 'Nepal', 'Pakistan', 'Russia', 'Tajikistan', 'Vietnam']", 
-    country_continent: "Asia", 
-    country_id: "CN", 
-    country_land_area: "9,326,410 (3,600,950)", 
-    country_latitude: 35.0, 
-    country_longitude: 105.0, 
-    country_name: "China", 
-    country_region: "East Asia & Pacific", 
-    country_water_area: "270,550 (104,460)", 
+    country_adjacent_countries:
+      "['Bhutan', 'Hong Kong', 'India', 'Kazakhstan', 'North Korea', 'Kyrgyzstan', 'Laos', 'Macao', 'Mongolia', 'Myanmar', 'Nepal', 'Pakistan', 'Russia', 'Tajikistan', 'Vietnam']",
+    country_continent: "Asia",
+    country_id: "CN",
+    country_land_area: "9,326,410 (3,600,950)",
+    country_latitude: 35.0,
+    country_longitude: 105.0,
+    country_name: "China",
+    country_region: "East Asia & Pacific",
+    country_water_area: "270,550 (104,460)",
     country_water_percent: "2.82",
-    country_topography_image: "http://www.vidiani.com/maps/maps_of_asia/maps_of_china/topographical_map_of_china.jpg"
+    country_topography_image:
+      "http://www.vidiani.com/maps/maps_of_asia/maps_of_china/topographical_map_of_china.jpg",
   },
   IN: {
-    country_adjacent_countries: "['Bangladesh', 'Bhutan', 'China', 'Myanmar', 'Nepal', 'Pakistan']", 
-    country_continent: "Asia", 
-    country_id: "IN", 
-    country_land_area: "2,973,190 (1,147,960)", 
-    country_latitude: 20.0, 
-    country_longitude: 77.0, 
-    country_name: "India", 
-    country_region: "South Asia", 
-    country_water_area: "314,073 (121,264)", 
+    country_adjacent_countries:
+      "['Bangladesh', 'Bhutan', 'China', 'Myanmar', 'Nepal', 'Pakistan']",
+    country_continent: "Asia",
+    country_id: "IN",
+    country_land_area: "2,973,190 (1,147,960)",
+    country_latitude: 20.0,
+    country_longitude: 77.0,
+    country_name: "India",
+    country_region: "South Asia",
+    country_water_area: "314,073 (121,264)",
     country_water_percent: "9.55",
-    country_topography_image: "http://www.vidiani.com/maps/maps_of_asia/maps_of_india/detailed_topographical_map_of_india.jpg"
+    country_topography_image:
+      "http://www.vidiani.com/maps/maps_of_asia/maps_of_india/detailed_topographical_map_of_india.jpg",
   },
 };
 
--- front-end/src/screens/Geography/GeographyInstance.js
+++ front-end/src/screens/Geography/GeographyInstance.js
@@ -8,17 +8,17 @@ import codeToCountry from "../../codeToCountry";
 
 const GeographyInstance = ({}) => {
   const { country } = useParams();
-  
+
   const data = GeographyData[country];
 
   const country_neighbors = data
-  ? convertStringArrayToArray(data.country_adjacent_countries)
-  : null;
+    ? convertStringArrayToArray(data.country_adjacent_countries)
+    : null;
 
   const defaultProps = {
     center: {
-      lat: data.country_latitude,
-      lng: data.country_longitude,
+      lat: data?.country_latitude ?? 0,
+      lng: data?.country_longitude ?? 0,
     },
     zoom: 4,
   };
@@ -31,89 +31,127 @@ const GeographyInstance = ({}) => {
           <a href={"/geography/"}>{"View All"}</a>
         </text>
       </div>
-      {!data ? (
-        <div style={{ marginTop: 40, marginLeft: 70 }}>
-          Data for this model instance is not present in Phase 1, will be
-          present in the next phase.
-        </div>
-      ) : (
       <div className="instancePage">
-        <h1 className="countryName">{data.country_name}</h1>
-        <div className="allInfo">
-        <div className="actualData">
-            <h3 className="subTitle">Location</h3>
-            <div className="location">
-              <text>
-                <b>Latitude:</b> {data.country_latitude}
-              </text>
-              <text>
-                <b>Longitude:</b> {data.country_longitude}
-              </text>
-              <text>
-                <b>Continent:</b> {data.country_continent}
-              </text>
-              <text>
-                <b>Region:</b> {data.country_region}
-              </text>
-            </div>
-            <h3 className="subTitle">Adjacent Countries</h3>
-            <div className="adjacent">
-              <text>
-                {country_neighbors.join(", ")}
-              </text>{" "}
-              <br />
-            </div>
-            <h3 className="subTitle">Area</h3>
-            <div className="area">
-              <text>
-                <b>Land Area in km<sup>2</sup> (mi<sup>2</sup>):</b> {data.country_land_area}
-              </text>{" "}
-              <br />
-              <text>
-                <b>Water Area in km<sup>2</sup> (mi<sup>2</sup>):</b> {data.country_water_area}
-              </text>
-              <br />
-              <text>
-                <b>Water Percent:</b> {data.country_water_percent}
-              </text>
+        {!data ? (
+          <div style={{ marginTop: 40 }}>
+            Data for this model instance is not present in Phase 1, will be
+            present in the next phase.
+          </div>
+        ) : (
+          <div>
+            <h1 className="countryName">{data.country_name}</h1>
+            <div className="allInfo">
+              <div className="actualData">
+                <h3 className="subTitle">Location</h3>
+                <div className="location">
+                  <text>
+                    <b>Latitude:</b> {data.country_latitude}
+                  </text>
+                  <text>
+                    <b>Longitude:</b> {data.country_longitude}
+                  </text>
+                  <text>
+                    <b>Continent:</b> {data.country_continent}
+                  </text>
+                  <text>
+                    <b>Region:</b> {data.country_region}
+                  </text>
+                </div>
+                <h3 className="subTitle">Adjacent Countries</h3>
+                <div className="adjacent">
+                  <text>
+                    {country_neighbors.map((country, idx) => {
+                      const countryCodeAndCountry = Object.entries(
+                        codeToCountry
+                      ).filter(([_, val]) => val == country);
+                      if (countryCodeAndCountry.length > 0)
+                        return (
+                          <a
+                            href={
+                              "/foodandtourism/" + countryCodeAndCountry[0][0]
+                            }
+                          >
+                            {country}
+                            {idx < country_neighbors.length - 1 && ", "}
+                          </a>
+                        );
+                      return (
+                        <text>
+                          {country}
+                          {idx < country_neighbors.length - 1 && ", "}
+                        </text>
+                      );
+                    })}
+                  </text>
+                  <p
+                    style={{
+                      fontSize: 12,
+                      color: "gray",
+                    }}
+                  >
+                    Find out more about the culture, food and tourism of
+                    countries in the same region!
+                  </p>
+                  <br />
+                </div>
+                <h3 className="subTitle">Area</h3>
+                <div className="area">
+                  <text>
+                    <b>
+                      Land Area in km<sup>2</sup> (mi<sup>2</sup>):
+                    </b>{" "}
+                    {data.country_land_area}
+                  </text>{" "}
+                  <br />
+                  <text>
+                    <b>
+                      Water Area in km<sup>2</sup> (mi<sup>2</sup>):
+                    </b>{" "}
+                    {data.country_water_area}
+                  </text>
+                  <br />
+                  <text>
+                    <b>Water Percent:</b> {data.country_water_percent}%
+                  </text>
+                </div>
+              </div>
+              <div className="linksToModules">
+                <h4> Interested to learn more about {data.country_name}?</h4>
+                <text>
+                  Check out the{" "}
+                  <a href={"/demographics/" + country}>
+                    {"Basic Info and Demographics"}
+                  </a>{" "}
+                  of this country
+                </text>
+                <br />
+                <text>
+                  Check out the{" "}
+                  <a href={"/foodandtourism/" + country}>
+                    {"Food and Tourism"}
+                  </a>{" "}
+                  of this country
+                </text>
+              </div>
+              <div style={{ height: "40vh", width: "81%", marginTop: -65 }}>
+                <GoogleMapReact
+                  bootstrapURLKeys={{
+                    key: "AIzaSyBTg1SVCHYOg71DzgOow9G1iYuEw3jtJQ4",
+                  }}
+                  defaultCenter={defaultProps.center}
+                  defaultZoom={defaultProps.zoom}
+                ></GoogleMapReact>
+                <img
+                  src={data.country_topography_image}
+                  width="550"
+                  height="300"
+                  style={{ marginTop: 15, marginRight: 25, marginBottom: 20 }}
+                />
+              </div>
             </div>
           </div>
-          <div className="linksToModules">
-          <h4> Interested to learn more about {data.country_name}?</h4>
-          <text>
-            Check out the{" "}
-            <a href={"/demographics/" + country}>
-              {"Basic Info and Demographics"}
-            </a>{" "}
-            of this country
-          </text>
-          <br />
-          <text>
-            Check out the{" "}
-            <a href={"/foodandtourism/" + country}>{"Food and Tourism"}</a> of
-            this country
-          </text>
-        </div>
-        <div style={{ height: "40vh", width: "81%", marginTop:-65 }}>
-            <GoogleMapReact
-              bootstrapURLKeys={{
-                key: "AIzaSyBTg1SVCHYOg71DzgOow9G1iYuEw3jtJQ4",
-              }}
-              defaultCenter={defaultProps.center}
-              defaultZoom={defaultProps.zoom}
-            ></GoogleMapReact>
-             <img
-              src={data.country_topography_image}
-              width="550"
-              height="300"
-              style={{ marginTop:15, marginRight: 25, marginBottom:20 }}
-              />
-        </div>
-    
-          
-        </div>
+        )}
       </div>
-      )}
     </div>
   );
 };
