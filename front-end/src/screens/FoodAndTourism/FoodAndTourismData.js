7bf0470fb734e42efb7e756d0d624d539490ff07
dany-torres <53352534+dany-torres@users.noreply.github.com>
2021-10-22 11:49:12 -0500

    removed country name from main dishes and attractions

--- front-end/src/screens/FoodAndTourism/FoodAndTourismData.js
+++ front-end/src/screens/FoodAndTourism/FoodAndTourismData.js
@@ -4,10 +4,10 @@ const FoodAndTourismData = {
     country_coldest_month_temp: 2.7,
     country_id: "NP",
     country_income_level: "Lower middle income",
-    country_main_attraction: "Annapurna Mountain Range, Nepal",
+    country_main_attraction: "Annapurna Mountain Range",
     country_main_attraction_image_src:
       "https://www.listchallenges.com/f/items/5c097bdb-e271-4e95-88e7-e9cd30bec571.jpg",
-    country_main_dishes: "['Nepal - Dal Bhat']",
+    country_main_dishes: "['Dal Bhat']",
     country_main_dishes_images:
       "['https://www.listchallenges.com/f/items/7874bc51-7a40-4390-833f-9648d6ee3d9f.jpg']",
     country_name: "Nepal",
@@ -30,7 +30,7 @@ const FoodAndTourismData = {
     country_main_attraction_image_src:
       "https://www.listchallenges.com/f/items/3df51874-03fa-460b-bcb1-53cd8af7516e.jpg",
     country_main_dishes:
-      "['China- Peking Duck', 'Chinese Noodles', 'Fried Rice', 'Dumplings', \"Family Style Red Braised Pork\"]",
+      "['Peking Duck', 'Chinese Noodles', 'Fried Rice', 'Dumplings', \"Family Style Red Braised Pork\"]",
     country_main_dishes_images:
       "['https://www.listchallenges.com/f/items/31694186-bbfe-47fd-88b0-edc7c91854d8.jpg', 'https://www.listchallenges.com/f/items/fe2ad318-c2c7-4530-84c1-844094e1271c.jpg', 'https://www.listchallenges.com/f/items/f40f918c-9af3-491c-afd7-8aa231401ee7.jpg', 'https://www.listchallenges.com/f/items/60f78c7b-bf9f-407a-945d-7822953f85c4.jpg', 'https://www.listchallenges.com/f/items/c02a7990-bfa1-449c-9e28-0ac2bc6f9042.jpg']",
     country_name: "China",
@@ -53,7 +53,7 @@ const FoodAndTourismData = {
     country_main_attraction_image_src:
       "https://www.listchallenges.com/f/items/4dd9d779-3bc9-49d8-89d5-a34b133c6ab3.jpg",
     country_main_dishes:
-      "['Samosa', 'Tandoori Chicken - India', 'Vindaloo', 'Dal Makhani', 'Rogan Josh']",
+      "['Samosa', 'Tandoori Chicken', 'Vindaloo', 'Dal Makhani', 'Rogan Josh']",
     country_main_dishes_images:
       "['https://www.listchallenges.com/f/items/1c71c549-0e97-4ba5-86cf-7b9bba483e56.jpg', 'https://www.listchallenges.com/f/items/d082ec54-722d-4e19-bb3c-1b054aff9436.jpg', 'https://www.listchallenges.com/f/items/d15d6a63-35c9-4e0d-93c8-d7f22389540e.jpg', 'https://www.listchallenges.com/f/items/642d7903-f8a8-491c-a61b-a922cce580ee.jpg', 'https://www.listchallenges.com/f/items/03bbfcb1-59f5-4e9e-bbed-435da2b2649c.jpg']",
     country_name: "India",
