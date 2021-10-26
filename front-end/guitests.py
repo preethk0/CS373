eb4b533809693fda0ee735cbde18628a711153a8
Mehul Daruka <mehul.daruka@utexas.edu>
2021-10-25 20:02:50 -0500

    selenium CI changes

--- front-end/guitests.py
+++ front-end/guitests.py
@@ -4,12 +4,11 @@ from sys import platform
 if __name__ == "__main__":
     # Use chromedriver based on OS
     if platform == "linux":
-        PATH = "/app/src/front-end/gui_tests/chromedriver_linux"
+        PATH = "./front-end/gui_tests/chromedriver_linux"
     elif platform == "darwin":
-        PATH = "/app/src/front-end/gui_tests/chromedriver_mac"
+        PATH = "./front-end/gui_tests/chromedriver_mac"
     else:
-        PATH = "/app/src/front-end/gui_tests/chromedriver.exe"
-        
+        PATH = "./front-end/gui_tests/chromedriver.exe"
 
     # Run all of the gui tests
     os.system("python3 ./front-end/gui_tests/demographicsTests.py " + PATH)
