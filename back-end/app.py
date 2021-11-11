6051143ec729ce7e84b81ea55a144ed35d1a7787
dany-torres <53352534+dany-torres@users.noreply.github.com>
2021-11-11 14:00:39 -0600

    added sorting/filtering calls, and implemented pagination

--- back-end/app.py
+++ back-end/app.py
@@ -1,5 +1,6 @@
 from flask import request, jsonify
 from demographics import filter_demographics, sort_demographics, search_demographics
+from geography import filter_geography, sort_geography, search_geography
 
 from models import (
     Demographics,
@@ -54,9 +55,23 @@ def get_demographics(id):
 # Retrieve geography data for all countries
 @app.route("/geography", methods=["GET"])
 def get_all_geography():
-    all_geography = Geography.query.all()
-    result = all_geography_schema.dump(all_geography)
-    return jsonify(result)
+    # all_geography = Geography.query.all()
+    # result = all_geography_schema.dump(all_geography)
+    # return jsonify(result)
+    queries = request.args.to_dict(flat=False)
+    geo_query = db.session.query(Geography)
+
+    page = int(queries['page'][0]) if "page" in queries else 1
+    per_page = int(queries['per_page'][0]) if "per_page" in queries else 10
+    
+    # geo_query = filter_geography(geo_query, queries)
+    geo_query = sort_geography(geo_query, queries)
+    # geo_query = search_geography(geo_query, queries)
+    geography = geo_query.paginate(page=page, per_page=per_page)
+
+    result = all_geography_schema.dump(geography.items, many=True)
+
+    return {'result': result, 'count': geography.count()}
 
 
 # Retrieve geography data for country with specific country-id
