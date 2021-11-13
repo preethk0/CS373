5d7102015616ae5424b2928ffca1d0df143ac52e
Mehul Daruka <mehul.daruka@utexas.edu>
2021-11-13 13:09:19 -0600

    Fix CI error and remove unnecessary imports

--- front-end/src/components/Cards/CountryCard.js
+++ front-end/src/components/Cards/CountryCard.js
@@ -1,8 +1,6 @@
 import React from "react";
 import { Card } from "antd";
 import "./Card.css";
-import { convertStringArrayToArray } from "../../utils";
-import Highlighter from "react-highlight-words";
 
 const CountryCard = ({ country, searchQuery }) => {
   const formatNumbers = (number) => {
