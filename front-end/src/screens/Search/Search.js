63a374d2ace05f07bfac65cbdc137d90b41f55fb
Justin Lee <justinolee@utexas.edu>
2021-11-15 13:50:51 -0600

    geography table showing

--- front-end/src/screens/Search/Search.js
+++ front-end/src/screens/Search/Search.js
@@ -1,27 +1,27 @@
 import React, { useEffect, useState, useMemo } from "react";
 import CountryCard from "../../components/Cards/CountryCard";
+import MaterialTable from "material-table";
 import { Pagination } from "@mui/material";
+import { TablePagination } from "@mui/material";
 import { Spinner } from "react-bootstrap";
+import * as Bootstrap from "react-bootstrap";
 import { demographicCountryNames } from "../../countryData/demographicsCountries";
+import { geographyCountryNames } from "../../countryData/geographyCountries";
 import { MDBInput } from "mdbreact";
+import {
+    useQueryParams,
+    StringParam,
+    NumberParam,
+    ArrayParam,
+    withDefault,
+  } from "use-query-params";
+  
 
 const axios = require("axios");
 
-const customStyles = {
-  menu: (provided, state) => ({
-    ...provided,
-    width: 200,
-    color: state.selectProps.menuColor,
-  }),
-  container: (provided) => ({
-    ...provided,
-    width: 200,
-    marginLeft: 10,
-    marginRight: 10,
-  }),
-};
 
 const Search = ({}) => {
+  const [geographyData, setGeographyData] = useState([]);
   const [demographicsData, setDemographicsData] = useState([]);
   const [tempSearch, setTempSearch] = useState("");
   const [itemCount, setItemCount] = useState(demographicCountryNames.length);
@@ -33,10 +33,14 @@ const Search = ({}) => {
     country_population: [],
     country_gdp: [],
     country_language: [],
+    country_longitude: withDefault(ArrayParam, []),
+    country_latitude: withDefault(ArrayParam, []),
+    country_continent: withDefault(ArrayParam, []),
+    country_region: withDefault(ArrayParam, []),
     sort: "",
     search: "",
   });
-
+  
   const updateParam = (key, value) => {
     const currentParams = params;
     setParams({
@@ -44,6 +48,39 @@ const Search = ({}) => {
       [key]: value,
     });
   };
+
+  const getGeography = (country) => {
+    const data = geographyData[country];
+    return (
+      <tr key={data.country_id}>
+        <td>
+          <a href={"/geography/" + data.country_id}>{highlightText(data.country_name)}</a>
+        </td>
+        <td> {highlightText(data.country_longitude.toString())}</td>
+        <td> {highlightText(data.country_latitude.toString())}</td>
+        <td> {highlightText(data.country_continent)}</td>
+        <td> {highlightText(data.country_region)}</td>
+      </tr>
+    );
+    };
+
+    const highlightText = (text) => {
+      const searchQuery = params.search?.toLowerCase() ?? "";
+      const parts = text.split(new RegExp(`(${searchQuery})`, "gi"));
+    
+        return (
+            <span>
+            {parts.map((part) =>
+                part.toLowerCase() === searchQuery ? (
+                <text style={{ backgroundColor: "yellow" }}>{part}</text>
+                ) : (
+                part
+            )
+            )}
+            </span>
+        );
+    };
+      
 
   var l = useEffect(() => {
     // Adapted from TexasVotes
@@ -77,8 +114,28 @@ const Search = ({}) => {
         });
       }
 
-      if (params.sort.length > 0) {
-        urlParams.append("sort", params.sort);
+      if (params.country_longitude.length > 0) {
+        params.country_longitude.forEach((long) => {
+          urlParams.append("country_longitude", long);
+        });
+      }
+
+      if (params.country_latitude.length > 0) {
+        params.country_latitude.forEach((lat) => {
+          urlParams.append("country_latitude", lat);
+        });
+      }
+
+      if (params.country_continent.length > 0) {
+        params.country_continent.forEach((continent) => {
+          urlParams.append("country_continent", continent);
+        });
+      }
+
+      if (params.country_region.length > 0) {
+        params.country_region.forEach((region) => {
+          urlParams.append("country_region", region);
+        });
       }
 
       if (params.search.length > 0) {
@@ -101,7 +158,21 @@ const Search = ({}) => {
         });
     };
 
+    const getGeographyData = async () => {
+        const urlParams = buildParams(params);
+        axios
+          .get(
+            "http://10.165.130.235:5000/geography?" + urlParams.toString()
+          )
+          .then((response) => {
+            setGeographyData(response.data.result);
+            setItemCount(response.data.count);
+            setLoading(false);
+          });
+      };  
+
     getDemographicsData();
+    getGeographyData();
   }, [params]);
 
   return (
@@ -159,7 +230,42 @@ const Search = ({}) => {
               />
             </div>
           </div>
+          <h2 className="header">Geography</h2>
+          <div>
+            <div style={{ paddingLeft: "865pt", paddingBottom: "2pt"}}>
+                {highlightText(
+                    `Displaying ${
+                    itemCount > 0 ? (params.page - 1) * 10 + 1 : 0
+                    }-${Math.min(params.page * 10, itemCount)} of ${itemCount}`
+                )}
+            </div>
+            
+            <Bootstrap.Table table-bordered>
+              <thead>
+                <tr>
+                  <th scope="col">Country</th>
+                  <th scope="col">Longitude</th>
+                  <th scope="col">Latitude</th>
+                  <th scope="col">Continent</th>
+                  <th scope="col">Region</th>
+              </tr>
+              </thead>
+              <tbody>{Object.keys(geographyData).map(getGeography)}</tbody>
+            </Bootstrap.Table>
+            <Pagination
+                defaultPage={1}
+                page={params.page}
+                onChange={(_, value) => updateParam("page", value)}
+                count={Math.ceil(itemCount / 10)}
+                variant="outlined"
+                color="primary"
+                showFirstButton 
+                showLastButton
+                style={{ paddingTop: "10pt", paddingLeft:"675pt"}}
+              />
+          </div>  
         </div>
+
       ) : (
         <Spinner
           animation="border"
@@ -167,6 +273,8 @@ const Search = ({}) => {
           style={{ marginTop: "15%", width: 60, height: 60 }}
         />
       )}
+
+      
     </div>
   );
 };
