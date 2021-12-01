f12bc88f15e8068599a2daecbab55d363b82e281
dany-torres <53352534+dany-torres@users.noreply.github.com>
2021-11-30 21:23:31 -0600

    Added provider's visualization, bubble graph for brands revenue

--- front-end/src/screens/Visualizations/ProviderVisualizations.js
+++ front-end/src/screens/Visualizations/ProviderVisualizations.js
@@ -1,13 +1,13 @@
-import React, { useEffect, useState, useMemo, useRef } from "react";
+import React, { useEffect, useState } from "react";
 import * as d3 from "d3";
 import ReactBubbleChart from "./ReactBubbleChart";
 import "./OurVisualizations.css";
 
 const axios = require("axios");
-const gdpFactor = 4 * Math.pow(10, 11);
+const revenueFactor = Math.pow(10, 9);
 
 const ProviderVisualizations = ({}) => {
-  const [geoData, setGeoData] = useState([]);
+  const [brandsData, setBrandsData] = useState([]);
 
   useEffect(() => {
     const buildParams = (num) => {
@@ -18,9 +18,67 @@ const ProviderVisualizations = ({}) => {
 
       return urlParams;
     };
+
+    const getBrandsData = async () => {
+      axios
+        .get(
+          "https://api.mytechreview.me/brands"
+      )
+      .then((response) => {
+        const data = response.data;
+        setBrandsData(data);
+      });
+    };
+
+    getBrandsData();
   }, []);
 
-  return <div></div>;
+
+  return (
+    <div>
+      <h2 className="header">Brand Revenue</h2>
+      <ReactBubbleChart
+        graph={{
+          zoom: 1,
+          offsetX: 0.35,
+          offsetY: 0,
+        }}
+        width={window.innerWidth * 0.4}
+        padding={0} // optional value, number that set the padding between bubbles
+        showLegend={false} // optional value, pass false to disable the legend.
+        legendFont={{
+          family: "Arial",
+          size: 12,
+          color: "#000",
+          weight: "bold",
+        }}
+        valueFont={{
+          family: "Verdana",
+          size: 12,
+          color: "#000",
+        }}
+        labelFont={{
+          family: "Verdana",
+          size: 10,
+          color: "#000",
+        }}
+        bubbleClickFunc={(id) => {
+          window.location = "https://www.mytechreview.me/#/brand/" + id;
+        }}
+        data={brandsData.map((brand) => {
+          let revenue = brand.revenue;
+          if (revenue == null) {
+             revenue = 0
+          }
+          return {
+            label: brand.name,
+            value: (revenue * revenueFactor),
+            id: brand.id,
+          };
+        })}
+      />
+    </div>
+  );
 };
 
 export default ProviderVisualizations;
