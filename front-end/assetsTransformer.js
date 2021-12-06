eb6ff7676ef6309b540fe93ced90656aa75ea825
Mehul Daruka <mehul.daruka@utexas.edu>
2021-12-06 12:17:47 -0600

    Test out Jest tests

---
+++ front-end/assetsTransformer.js
@@ -0,0 +1,9 @@
+/* For jest tests */
+
+const path = require("path");
+
+module.exports = {
+  process(src, filename, config, options) {
+    return "module.exports = " + JSON.stringify(path.basename(filename)) + ";";
+  },
+};
